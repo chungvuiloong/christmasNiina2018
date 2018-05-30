184fe91fe5d97bd5f0d1f9d7530cd36bb17915db
jirimicvl <35882808+jirimicvl@users.noreply.github.com>
2018-05-30 12:02:33 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -21,6 +21,7 @@ function meowUs() {
           // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
