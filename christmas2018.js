1335b3893bd70dceec364c5d0b857e70551c07e9
jirimicvl <35882808+jirimicvl@users.noreply.github.com>
2018-12-08 19:08:55 +0800

    Update christmas2018.js

--- christmas2018.js
+++ christmas2018.js
@@ -32,3 +32,13 @@
 
     }
 }, 1000);
+
+
+function disableClick(){
+        document.onclick=function(event){
+          if (event.button == 2) {
+            alert('Right Click Message');
+            return false;
+          }
+        }
+      }
