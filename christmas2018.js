c0b288212394bcbc1238b7354fd98e73f434ea88
jirimicvl <35882808+jirimicvl@users.noreply.github.com>
2018-12-16 16:42:28 +0800

    Add files via upload

--- christmas2018.js
+++ christmas2018.js
@@ -5,13 +5,17 @@
     // Set the date we're counting down to
     var countDownDate = new Date("Dec 24, 2018 21:00:00 GMT+0200").getTime();
 
+    var loveMusic = document.getElementById("loveMusic");
+
         // Update the count down every 1 second
         var x = setInterval(function() {
 
             // Get todays date and time
             var now = new Date().getTime();
+
             // Find the distance between now and the count down date
             var distance = countDownDate - now;
+            
             // Time calculations for days, hours, minutes and seconds
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -24,11 +28,9 @@
             // If the count down is over, write some text
             if (distance < 0) {
                 clearInterval(x);
+                document.getElementById("loveMusic").play();
                 document.getElementById("display").style.visibility = "hidden";
                 document.getElementById("content").style.visibility = "visible";
-
-
-
 
     }
 }, 1000);
